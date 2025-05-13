24d34968a5cc3351996627a95e24016a8f95e4ac
Chloé <152994537+mangoshaker@users.noreply.github.com>
2025-05-13 22:59:01 +0100

    Retrait des namespaces dans les classes

--- pokemon/pokemon.cpp
+++ pokemon/pokemon.cpp
@@ -5,65 +5,65 @@
 
 using namespace std;
 
-namespace pokemon
-{
-    Pokemon::Pokemon(std::string Nom, int HP, std::string attaque, int degat, std::string type1, std::string type2) {
-        this->nom = Nom;
-        this->HP = HP;
-        this->attaque = attaque;
-        this->degat = degat;
-        types.push_back(type1);
-        if (!type2.empty()) types.push_back(type2);
-    }
+using namespace pokemon;
 
-    /**
-     * @brief Retourne les types du pokemon
-     */
-    const vector<string>& Pokemon::getTypes() const{
-        return this->types;
-    }
-    const string Pokemon::getNom() const {
-        return nom;
-    }
-
-    const int Pokemon::getHP()const{
-        return HP;
-    }
-    const int Pokemon::getDegat()const{
-        return degat;
-    }
-
-    /**
-     *  @brief affiche une phrase signifiant l'attaque - fct peut etre à modifier
-     */
-    void Pokemon::attaquer(Pokemon* cible) {
-        cout << nom << " attaque " << cible->nom << " avec " << attaque << " !" << endl;
-    }
-
-    /**
-     * @brief affiche un petit message sur le pokemon
-     * @return string contenant le message
-     */
-    string Pokemon::interagir()
-    {
-        string message = nom.substr(0, 4) + nom.substr(0, 4) + ", je suis " + nom + "!";
-        cout << message << endl;
-        return message;
-    }
-
-    /**
-     * @brief affiche les infos des pokemons
-     */
-    void Pokemon::infoPokemon()
-    {
-        cout << "Nom: " << nom << ", HP: " << HP << ", Attaque: " << attaque << ", Degats: " << degat << endl;
-        for (string s : types) {
-            cout << "Type: " << s << endl;
-        }
-    }
-
-    Pokemon::~Pokemon() {
-    }
-    
+Pokemon::Pokemon(std::string Nom, int HP, std::string attaque, int degat, std::string type1, std::string type2) {
+    this->nom = Nom;
+    this->HP = HP;
+    this->attaque = attaque;
+    this->degat = degat;
+    types.push_back(type1);
+    if (!type2.empty()) types.push_back(type2);
 }
+
+/**
+ * @brief Retourne les types du pokemon
+ */
+const vector<string>& Pokemon::getTypes() const{
+    return this->types;
+}
+const string Pokemon::getNom() const {
+    return nom;
+}
+
+const int Pokemon::getHP()const{
+    return HP;
+}
+const int Pokemon::getDegat()const{
+    return degat;
+}
+
+/**
+ *  @brief affiche une phrase signifiant l'attaque - fct peut etre à modifier
+ */
+void Pokemon::attaquer(Pokemon* cible) {
+    cout << nom << " attaque " << cible->nom << " avec " << attaque << " !" << endl;
+}
+
+/**
+ * @brief affiche un petit message sur le pokemon
+ * @return string contenant le message
+ */
+string Pokemon::interagir()
+{
+    string message = nom.substr(0, 4) + nom.substr(0, 4) + ", je suis " + nom + "!";
+    cout << message << endl;
+    return message;
+}
+
+/**
+ * @brief affiche les infos des pokemons
+ */
+void Pokemon::infoPokemon()
+{
+    cout << "Nom: " << nom << ", HP: " << HP << ", Attaque: " << attaque << ", Degats: " << degat << endl;
+    for (string s : types) {
+        cout << "Type: " << s << endl;
+    }
+}
+
+Pokemon::~Pokemon() {
+}
+
+
 
--- pokemon/typeChart.cpp
+++ pokemon/typeChart.cpp
@@ -7,79 +7,77 @@
 
 using namespace std;
 using namespace pokemon;
- namespace typechart
-{
-    map<pair<string, string>, float> tablemult;
+using namespace typechart;
+map<pair<string, string>, float> tablemult;
 
-    TypeChart::TypeChart(){
-        creationTypeChart("pokemon/Ressources/tabfaiblesseresistance.csv");
+TypeChart::TypeChart(){
+    creationTypeChart("pokemon/Ressources/tabfaiblesseresistance.csv");
+}
+
+/**
+ * @brief Remplie la map des faiblesses/résistances
+ */
+void TypeChart::creationTypeChart(const string& filename) {
+    ifstream fichier(filename);
+    if (!fichier.is_open()) {
+        cerr << "Erreur : impossible d'ouvrir le fichier " << filename << endl;
+        return;
     }
 
-    /**
-     * @brief Remplie la map des faiblesses/résistances
-     */
-    void TypeChart::creationTypeChart(const string& filename) {
-        ifstream fichier(filename);
-        if (!fichier.is_open()) {
-            cerr << "Erreur : impossible d'ouvrir le fichier " << filename << endl;
-            return;
-        }
+    string ligne;
+    getline(fichier, ligne); // Lire l'en-tête et l'ignorer
 
-        string ligne;
-        getline(fichier, ligne); // Lire l'en-tête et l'ignorer
-
-        while (getline(fichier, ligne)) {
-            stringstream ss(ligne);
-            string attaquant, defenseur, multiplicateurStr;
-            
-            // Lire les trois colonnes séparées par des virgules
-            getline(ss, defenseur, ',');
-            getline(ss, attaquant, ',');
-            getline(ss, multiplicateurStr, ',');
-
-            float multiplicateur = stof(multiplicateurStr);
-
-            tablemult[{attaquant, defenseur}] = multiplicateur;
-        }
-
-        fichier.close();
-    }
-
-    /**
-     * @brief Affiche TypeChart
-     */
-    void TypeChart::afficherTypeChart() {
-        for (const auto& entry : tablemult) {
-            cout << entry.first.first << " -> " << entry.first.second << " : " << entry.second << endl;
-        }
-    }
-
-    /**
-     * @brief Renvoie le multiplicateur de dégats en fonction des types du pokemon attaquant et ceux du defenseur
-     */
-    float TypeChart::getMult(Pokemon* attaquant, Pokemon* defenseur){
-        float mult = 1;
-        const vector<string>& typesAtt = attaquant->getTypes();
-        const vector<string>& typesDef = defenseur->getTypes();
-
-        for (const string& typeA : typesAtt){
-            for(const string& typeD: typesDef){
-                mult*=getMultIndiv(typeA,typeD);
-            }
-        }
+    while (getline(fichier, ligne)) {
+        stringstream ss(ligne);
+        string attaquant, defenseur, multiplicateurStr;
         
-        return mult;
+        // Lire les trois colonnes séparées par des virgules
+        getline(ss, defenseur, ',');
+        getline(ss, attaquant, ',');
+        getline(ss, multiplicateurStr, ',');
+
+        float multiplicateur = stof(multiplicateurStr);
+
+        tablemult[{attaquant, defenseur}] = multiplicateur;
     }
 
-    /**
-     * @brief renvoie le multiplicateur associé à deux types
-     */
-    float TypeChart::getMultIndiv(const string& type_att, const string& type_def) {
-        auto it = tablemult.find({type_att, type_def});
-        if (it != tablemult.end()) {
-            return it->second;
-        } else {
-            return 1.0f; // Pas de faiblesse ou de résistance particulière
+    fichier.close();
+}
+
+/**
+ * @brief Affiche TypeChart
+ */
+void TypeChart::afficherTypeChart() {
+    for (const auto& entry : tablemult) {
+        cout << entry.first.first << " -> " << entry.first.second << " : " << entry.second << endl;
+    }
+}
+
+/**
+ * @brief Renvoie le multiplicateur de dégats en fonction des types du pokemon attaquant et ceux du defenseur
+ */
+float TypeChart::getMult(Pokemon* attaquant, Pokemon* defenseur){
+    float mult = 1;
+    const vector<string>& typesAtt = attaquant->getTypes();
+    const vector<string>& typesDef = defenseur->getTypes();
+
+    for (const string& typeA : typesAtt){
+        for(const string& typeD: typesDef){
+            mult*=getMultIndiv(typeA,typeD);
         }
+    }
+    
+    return mult;
+}
+
+/**
+ * @brief renvoie le multiplicateur associé à deux types
+ */
+float TypeChart::getMultIndiv(const string& type_att, const string& type_def) {
+    auto it = tablemult.find({type_att, type_def});
+    if (it != tablemult.end()) {
+        return it->second;
+    } else {
+        return 1.0f; // Pas de faiblesse ou de résistance particulière
     }
 }
